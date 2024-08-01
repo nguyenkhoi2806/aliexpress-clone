4ae704be7e06ba5592edc51a974fd1d43959b4d3
khoilen <khoi.lenguyen@evolutionwellnesssolutionsg.onmicrosoft.com>
2024-08-01 13:10:59 +0700

    feat: add feat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  pages: true,
+  modules: [
+    'nuxt-icon',
+    'nuxt-lodash',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxt/tailwindcss',
+    '@nuxt/supabase',
+  ]
 })

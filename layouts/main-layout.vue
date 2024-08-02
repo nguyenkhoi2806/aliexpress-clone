bace7b1429c24e83cc1d4fb49221966f22dcf476
khoilen <khoi.lenguyen@evolutionwellnesssolutionsg.onmicrosoft.com>
2024-08-02 16:04:15 +0700

    feat: update top menu

--- layouts/main-layout.vue
+++ layouts/main-layout.vue
@@ -25,12 +25,40 @@
                     <Icon name="ph:user-thin" size="17" />
                     Account
                     <Icon name="mdi:chevron-down" size="15" class="ml-5" />
+                    <div id="AccountMenu" v-if="isAccountMenu"
+                        class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b ">
+                        <div v-if="true">
+                            <div class="text-semibold text-[15px] my-4 px-3">Welcome to AliExpress!</div>
+                            <div class="flex items-center gap-1 px-3 mb-3">
+                                <NuxtLink to="/auth"
+                                    class="bg-[#FF4646] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2">
+                                    Login / Register
+                                </NuxtLink>
+                            </div>
+                        </div>
+                        <div class="border-b" />
+                        <ul class="bg-white">
+                            <li @click="navigateTo('/orders')" class="text-[13px] py-2 px-4 w-full hover:bg-gray-200">
+                                My Orders
+                            </li>
+                            <li v-if="true" class="text-[13px] py-2 px-4 w-full hover:bg-gray-200">
+                                Sign out
+                            </li>
+                        </ul>
+                    </div>
                 </li>
             </ul>
+        </div>
+        <div id="MainHeader" class="flex items-center w-full bg-white">
+            <div class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto">
+                <NuxtLink to="/" class="min-w-[170px]">
+                    <img width="170" src="/AliExpress-logo.png" />
+                </NuxtLink>
+            </div>
         </div>
     </div>
 </template>
 
-<script>
+<script setup>
 let isAccountMenu = ref(false)
 </script>

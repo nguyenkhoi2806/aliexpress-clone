f1d803925a578e437a2c07e55d10330eb5087ab1
khoilen <khoi.lenguyen@evolutionwellnesssolutionsg.onmicrosoft.com>
2024-08-05 17:12:46 +0700

    fix: layout item page

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -2,9 +2,9 @@
     <MainLayout>
         <div id="ItemPage" class="mt-4 max-w-[1200px] mx-auto px-2">
             <div class="md:flex gap-4 justify-between mx-auto w-full">
-                <div class="md:w-[40%]">
-                    <img class="rounded-lg object-fit" src="https://picsum.photos/id/77/200/300" />
-                    <div v-if="images[0] !== ''" class="flex items-center justify-center mt-2">
+                <div class="md:w-[40%] flex flex-col gap-2">
+                    <img class="rounded-lg object-fit" src="https://picsum.photos/id/77/400/300" />
+                    <div v-if="images[0] !== ''" class="flex items-center justify-center gap-3">
                         <div v-for="image in images" :key="image">
                             <img @mouseover="currentImage = image" @click="currentImage = image" width="70"
                                 class="rounded-md object-fit border-[3px] cursor-pointer"

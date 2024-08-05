47d71e1bec221894bf7d22fd5fc6710fb3773fee
khoilen <khoi.lenguyen@evolutionwellnesssolutionsg.onmicrosoft.com>
2024-08-05 19:05:34 +0700

    feat: add shopping cart page

---
+++ pages/shopping-cart.vue
@@ -0,0 +1,11 @@
+<template>
+    <MainLayout>
+        <div id="ShoppingCartPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+    import MainLayout from '~/layouts/main-layout.vue';
+</script>

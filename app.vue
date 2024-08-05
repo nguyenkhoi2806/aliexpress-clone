a8784a1be6ed0f813278dd1bc4a014bff9400b5d
khoilen <khoi.lenguyen@evolutionwellnesssolutionsg.onmicrosoft.com>
2024-08-05 19:02:17 +0700

    feat: update item page

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bg-[#FAFAFA]">
     <NuxtPage />
   </div>
 </template>
--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -21,6 +21,48 @@
                             Description Section
                         </p>
                     </div>
+                    <div class="flex items-center pt-1.5">
+                        <span class="h-4 min-w-4 rounded-full p-0.5 bg-[#FFD000] mr-2">
+                            <Icon name="material-symbols:star-rounded" class="-mt-[17px]" size="12" />
+                        </span>
+                        <p class="text-[#FF5353]">Extra 5% off</p>
+                    </div>
+                    <div class="flex items-center justify-start my-2">
+                        <Icon name="ic:baseline-star" color="#FF5353" />
+                        <Icon name="ic:baseline-star" color="#FF5353" />
+                        <Icon name="ic:baseline-star" color="#FF5353" />
+                        <Icon name="ic:baseline-star" color="#FF5353" />
+                        <Icon name="ic:baseline-star" color="#FF5353" />
+                        <span class="text-[13px] font-light ml-2">5 213 Reviews 1,000+ orders</span>
+                    </div>
+                    <div class="border-b"></div>
+
+                    <div class="flex items-center justify-start gap-2 my-2">
+                        <div class="text-xl font-bold">$ {{ priceComputed }}</div>
+                        <span class="bg-[#F5F5F5] border text-[#C08562] text-[9px] font-semibold px-1.5 rounded-sm">70%
+                            off</span>
+                    </div>
+                    <p class="text-[#009A66] text-xs font-semibold pt-1">
+                        Free 11-day delivery over ￡8.28
+                    </p>
+                    <p class="text-[#009A66] text-xs font-semibold pt-1">
+                        Free Shipping
+                    </p>
+                    <div class="py-2" />
+                    <button @click="addToCart()" :disabled="isInCart" class="
+                            px-6 
+                            py-2 
+                            rounded-lg 
+                            text-white 
+                            text-lg 
+                            font-semibold 
+                            bg-gradient-to-r 
+                            from-[#FF851A] 
+                            to-[#FFAC2C]
+                        ">
+                        <div v-if="isInCart">Is Added</div>
+                        <div v-else>Add to Cart</div>
+                    </button>
                 </div>
             </div>
         </div>
@@ -29,6 +71,11 @@
 
 <script setup>
 import MainLayout from '~/layouts/main-layout.vue';
+import { useUserStore } from '~/stores/user';
+
+const userStore = useUserStore();
+const route = useRoute();
+
 let currentImage = ref(null)
 const images = ref([
     'https://picsum.photos/id/77/200/300',
@@ -44,5 +91,18 @@ onMounted(() => {
         images[0] = 'https://picsum.photos/id/77/200/300'
     });
 })
+
+const isInCart = computed(() => {
+    return userStore.cart.some(product =>
+        route.params.id === product.id);
+})
+
+const priceComputed = computed(() => {
+    return '26.40'
+})
+
+const addToCart = () => {
+    alert('ADDED');
+}
 
 </script>

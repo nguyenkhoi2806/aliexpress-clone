75107065145a47b88512197a08575f82787d0e90
khoilen <khoi.lenguyen@evolutionwellnesssolutionsg.onmicrosoft.com>
2024-08-07 15:40:53 +0700

    feat: update shoppingcart page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
     <MainLayout>
         <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-            <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
-                <div v-if="products" v-for="product in products" :key="product">
+            <div v-if="products.length > 0"
+                class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+                <div v-for="product in products" :key="product">
                     <ProductCard :product="product" />
                 </div>
             </div>
--- pages/shopping-cart.vue
+++ pages/shopping-cart.vue
@@ -1,7 +1,7 @@
 <template>
     <MainLayout>
         <div id="ShoppingCartPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-            <div v-if="true" class="h-[500px] flex items-center justify-center flex-col">
+            <div v-if="false" class="h-[500px] flex items-center justify-center flex-col">
                 <div class="pt-20"></div>
                 <img class="mx-auto" width="250" src="/cart-empty.png" />
                 <div class="text-xl text-center mt-4">
@@ -12,7 +12,7 @@
                                 bg-[#FD374F] 
                                 w-full 
                                 text-white 
-                                text-[21px] 
+                                text-[18px] 
                                 font-semibold 
                                 p-1.5 
                                 rounded-full
@@ -22,10 +22,47 @@
                     </NuxtLink>
                 </div>
             </div>
+            <div v-if="true" class="md:flex gap-4 justify-between mx-auto w-full">
+                <div class="md:w-[65%]">
+                    <div class="bg-white rounded-lg p-4">
+                        <div class="text-2xl font-bold mb-2">
+                            Shopping Cart (0)
+                        </div>
+                    </div>
+                    <div class="bg-[#FEEEEF] rounded-lg p-4 mt-4">
+                        <div class="text-red-500 font-bold">
+                            Welcome Deal applicable on 1 item only
+                        </div>
+                    </div>
+                    <div id="Items" class="bg-white rounded-lg p-4 mt-4">
+                        <div v-for="product in products" v-bind:key="product.id">
+                            <CartItem :product="product" :selectedArray="selectedArray"
+                                @selectedRadio="selectedRadioFunc" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from '~/layouts/main-layout.vue';
+const products = [
+    {
+        "id": 1,
+        "title": "Product 1",
+        "description": "Description 1",
+        "price": 100,
+        "url": "https://picsum.photos/200/300"
+    },
+    {
+        "id": 2,
+        "title": "Product 2",
+        "description": "Description 2",
+        "price": 200,
+        "url": "https://picsum.photos/200/301"
+    },
+]
+
 </script>

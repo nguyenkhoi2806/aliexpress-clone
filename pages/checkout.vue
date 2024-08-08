52c6ae78b53b2f0910e25954774d441b8468b08c
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2024-08-08 20:02:07 +0700

    feat: add checkout page

---
+++ pages/checkout.vue
@@ -0,0 +1,29 @@
+<template>
+  <MainLayout>
+    <div></div>
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '~/layouts/main-layout.vue';
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore();
+const route = useRoute();
+
+let stripe = null;
+let elements = null;
+let card = null;
+let form = null;
+let total = ref(0);
+let clientSecret = null;
+let currentAddress = ref(null);
+let isProcessing = ref(false);
+
+onMounted(async () => {
+  isProcessing.value = true;
+
+  userStore.checkout.forEach((item) => {
+    total.value += item.price;
+  });
+});
+</script>

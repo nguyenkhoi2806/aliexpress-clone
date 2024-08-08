7f3f23db3517d94cf27433ca391be718494766ba
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2024-08-08 20:10:12 +0700

    feat: update checkout page

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,6 +1,23 @@
 <template>
   <MainLayout>
-    <div></div>
+    <div id="CheckoutPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div class="md:flex gap-4 justify-between mx-auto w-full">
+        <div class="md:w-[65%]">
+          <div class="bg-white rounded-lg p-4">
+            <div class="text-xl font-semibold mb-2">Shipping Address</div>
+            <div v-if="true">
+              <NuxtLink
+                to="/address"
+                class="flex items-center pb-2 text-blue-500 hover:text-red-400"
+              >
+                <Icon name="mdi:plus" size="18" class="mr-2" />
+                Update Address
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </MainLayout>
 </template>
 
@@ -26,4 +43,58 @@ onMounted(async () => {
     total.value += item.price;
   });
 });
+
+watch(
+  () => total.value,
+  () => {
+    if (total.value > 0) {
+      stripeInit();
+    }
+  }
+);
+
+const stripeInit = async () => {
+  const runtimeConfig = useRuntimeConfig();
+  stripe = Stripe(runtimeConfig.stripePk);
+
+  let res = await $fetch('/api/stripe/paymentintent', {
+    method: 'POST',
+    body: {
+      amount: total.value,
+    },
+  });
+  clientSecret = res.client_secret;
+
+  elements = stripe.elements();
+  const style = {
+    base: {
+      fontSize: '18px',
+    },
+    invalid: {
+      fontFamily: 'Arial, sans-serif',
+      color: '#EE4B2B',
+      iconColor: '#EE4B2B',
+    },
+  };
+  card = elements.create('card', {
+    hidePostalCode: true,
+    style: style,
+  });
+
+  card.mount('#card-element');
+  card.on('change', function (event) {
+    document.querySelector('button').disabled = event.empty;
+    document.querySelector('#card-error').textContent = event.error
+      ? event.error.message
+      : '';
+  });
+
+  isProcessing.value = false;
+};
+
+const pay = async () => {};
+
+const createOrder = () => {};
+
+const showError = (errorMsgText) => {};
 </script>

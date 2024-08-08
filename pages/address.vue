0cc34d82b0ddbb734086d8d46d27facb2e6a29d0
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2024-08-08 20:36:02 +0700

    feat: update address page

---
+++ pages/address.vue
@@ -0,0 +1,16 @@
+<template>
+  <MainLayout>
+    <div id="AddressPage" class="mt-4 max-w-[500px] mx-auto px-2">
+      <div class="mx-auto bg-white rounded-lg p-3">
+        <div class="text-xl text-bold mb-2">Address Details</div>
+        <form @submit.prevent="submit()"></form>
+      </div>
+    </div>
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '~/layouts/main-layout.vue';
+
+const submit = async () => {};
+</script>

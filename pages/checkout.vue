bed5067867b62dda4978a9f164e127b81e3fd95a
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2024-08-08 20:30:36 +0700

    feat: add checkout page

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -40,12 +40,60 @@
               </div>
             </div>
             <NuxtLink
+              v-else
               to="/address"
               class="flex items-center text-blue-500 hover:text-red-400"
             >
               <Icon name="mdi:plus" size="18" class="mr-2" />
               Add New Address
             </NuxtLink>
+          </div>
+          <div id="Items" class="bg-white rounded-lg p-4 mt-4">
+            <div v-for="product in products">
+              <CheckoutItem :product="product" />
+            </div>
+          </div>
+        </div>
+        <div class="md:w-[35%]">
+          <div id="PlaceOrder" class="bg-white rounded-lg p-4">
+            <div class="text-2xl font-extrabold mb-2">Summary</div>
+            <div class="flex items-center justify-between my-4">
+              <div class="">Total Shipping</div>
+              <div class="">Free</div>
+            </div>
+            <div class="border-t" />
+            <div class="flex items-center justify-between my-4">
+              <div class="font-semibold">Total</div>
+              <div class="text-2xl font-semibold">
+                $ <span class="font-extrabold">{{ total / 100 }}</span>
+              </div>
+            </div>
+            <form @submit.prevent="pay()">
+              <div
+                class="border border-gray-500 p-2 rounded-sm"
+                id="card-element"
+              />
+              <p
+                id="card-error"
+                role="alert"
+                class="text-red-700 text-center font-semibold"
+              />
+              <button
+                :disabled="isProcessing"
+                type="submit"
+                class="mt-4 bg-gradient-to-r from-[#FE630C] to-[#FF3200] w-full text-white text-[21px] font-semibold p-1.5 rounded-full"
+                :class="isProcessing ? 'opacity-70' : 'opacity-100'"
+              >
+                <Icon v-if="isProcessing" name="eos-icons:loading" />
+                <div v-else>Place order</div>
+              </button>
+            </form>
+          </div>
+          <div class="bg-white rounded-lg p-4 mt-4">
+            <div class="text-lg font-semibold mb-2 mt-2">AliExpress</div>
+            <p class="my-2">
+              AliExpress keeps your information and payment safe
+            </p>
           </div>
         </div>
       </div>
@@ -92,4 +140,21 @@ const pay = async () => {};
 const createOrder = () => {};
 
 const showError = (errorMsgText) => {};
+
+const products = [
+  {
+    id: 1,
+    title: 'Product 1',
+    description: 'Description 1',
+    price: 100,
+    url: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 2,
+    title: 'Product 2',
+    description: 'Description 2',
+    price: 200,
+    url: 'https://picsum.photos/200/301',
+  },
+];
 </script>

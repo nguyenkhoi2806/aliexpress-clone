bfbeb73da0f094c5f47ac45598d9e12357f603cd
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2024-08-10 15:50:22 +0700

    feat: add menu overlay

--- app.vue
+++ app.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="bg-[#FAFAFA]">
-    <NuxtPage />
-  </div>
+  <div class="bg-[#FAFAFA]" />
+  <NuxtPage />
+  <MenuOverlay
+    :class="[
+      {
+        'max-h-[100vh] transition-all duration-200 ease-in visible':
+          userStore.isMenuOverlay,
+      },
+      {
+        'max-h-0 transition-all duration-200 ease-out invisible':
+          !userStore.isMenuOverlay,
+      },
+    ]"
+  />
 </template>
+
+<script setup>
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore();
+const route = useRoute();
+
+let windowWidth = ref(process.client ? window.innerWidth : '');
+
+onMounted(() => {
+  userStore.isLoading = true;
+  window.addEventListener('resize', function () {
+    windowWidth.value = window.innerWidth;
+  });
+});
+
+watch(
+  () => windowWidth.value,
+  () => {
+    if (windowWidth.value >= 767) {
+      userStore.isMenuOverlay = false;
+    }
+  }
+);
+
+watch(
+  () => route.fullPath,
+  () => {
+    userStore.isLoading = true;
+  }
+);
+</script>
--- layouts/main-layout.vue
+++ layouts/main-layout.vue
@@ -149,6 +149,7 @@
           </button>
         </NuxtLink>
         <button
+          @click="userStore.isMenuOverlay = true"
           class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200"
         >
           <Icon name="radix-icons:hamburger-menu" size="33" />

6790b79c3533e94a3ffe773fd70845a5ae9df138
khoilen <khoi.lenguyen@evolutionwellnesssolutionsg.onmicrosoft.com>
2024-08-17 15:30:58 +0700

    feat: add seed data

--- server/api/get-all-product.js
+++ server/api/get-all-product.js
@@ -2,6 +2,8 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
-  const products = await prisma.products.findMany()
+  const products = await prisma.products.findMany({
+    take: 24
+  })
   return products;
 });

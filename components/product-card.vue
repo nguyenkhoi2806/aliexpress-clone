52a527163ba830144dbb84259fc3a2e226e25233
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2024-08-25 17:13:28 +0700

    feat: update image loading

--- components/product-card.vue
+++ components/product-card.vue
@@ -4,7 +4,11 @@
     class="bg-white border inline-block rounded hover:shadow-[0_0_10px_3px_rgba(0,0,0,0.15)] cursor-pointer"
   >
     <NuxtLink :to="`/item/${product.id}`">
-      <img class="rounded-t" :src="product.url" />
+      <NuxtImg
+        :src="product.url"
+        class="rounded-t"
+        placeholder="/placeholder-image.jpg"
+      />
 
       <div id="ProductDetails" class="p-2">
         <span class="flex items-center justify-start gap-3 px-1 pt-1">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     '@pinia-plugin-persistedstate/nuxt',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/supabase',
+    '@nuxt/image',
   ],
   runtimeConfig: {
     public: {

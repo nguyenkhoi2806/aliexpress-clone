867dd05d7376d20d370876798c1e441560b3b2d8
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2024-08-30 12:34:29 +0700

    feat: refactor app.vue

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,16 @@
-FROM node:20.16-alpine
+FROM node:18-alpine3.16
 
 WORKDIR /usr/src/app
 
 RUN npm install -g pnpm
 
-RUN npm install -g prisma
-
 COPY package.json pnpm-lock.yaml ./
 
-COPY prisma ./prisma/
-
-RUN pnpm install --frozen-lockfile
+RUN pnpm install --frozen-lockfile 
 
 COPY . .
 
-RUN prisma generate && pnpm run build
+RUN npx prisma generate && pnpm run build
 
 EXPOSE 3000
 
--- app.vue
+++ app.vue
@@ -1,50 +1,57 @@
 <template>
   <div>
     <div class="bg-[#FAFAFA]" />
-    <MainLayout>
+    <MainLayout v-if="!isAuthRoute">
       <NuxtPage />
     </MainLayout>
-    <MenuOverlay :class="[
-      {
-        'max-h-[100vh] transition-all duration-200 ease-in visible':
-          userStore.isMenuOverlay,
-      },
-      {
-        'max-h-0 transition-all duration-200 ease-out invisible':
-          !userStore.isMenuOverlay,
-      },
-    ]" />
+    <NuxtPage v-else />
+    <MenuOverlay
+      :class="[
+        {
+          'max-h-[100vh] transition-all duration-200 ease-in visible':
+            userStore.isMenuOverlay,
+        },
+        {
+          'max-h-0 transition-all duration-200 ease-out invisible':
+            !userStore.isMenuOverlay,
+        },
+      ]"
+    />
   </div>
 </template>
 
 <script setup>
-import { useUserStore } from '~/stores/user';
-import MainLayout from './layouts/main-layout.vue';
-const userStore = useUserStore();
-const route = useRoute();
+  import { useUserStore } from '~/stores/user';
+  import MainLayout from './layouts/main-layout.vue';
+  const userStore = useUserStore();
+  const route = useRoute();
 
-const windowWidth = ref(process.client ? window.innerWidth : '');
-
-onMounted(() => {
-  userStore.isLoading = true;
-  window.addEventListener('resize', function () {
-    windowWidth.value = window.innerWidth;
+  const isAuthRoute = computed(() => {
+    return ['/auth'].includes(route.path);
   });
-});
 
-watch(
-  () => windowWidth.value,
-  () => {
-    if (windowWidth.value >= 767) {
-      userStore.isMenuOverlay = false;
-    }
-  }
-);
+  const windowWidth = ref(process.client ? window.innerWidth : '');
 
-watch(
-  () => route.fullPath,
-  () => {
+  onMounted(() => {
     userStore.isLoading = true;
-  }
-);
+    window.addEventListener('resize', function () {
+      windowWidth.value = window.innerWidth;
+    });
+  });
+
+  watch(
+    () => windowWidth.value,
+    () => {
+      if (windowWidth.value >= 767) {
+        userStore.isMenuOverlay = false;
+      }
+    }
+  );
+
+  watch(
+    () => route.fullPath,
+    () => {
+      userStore.isLoading = true;
+    }
+  );
 </script>

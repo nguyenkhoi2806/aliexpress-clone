4d962d607e08b768c80a511db973ff2a3bca6021
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2024-09-16 15:17:28 +0700

    feat: update husky precommit

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,12 +1,22 @@
 <template>
-  <div id="CheckoutPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+  <div
+    id="CheckoutPage"
+    class="mt-4 max-w-[1200px] mx-auto px-2"
+  >
     <div class="md:flex gap-4 justify-between mx-auto w-full">
       <div class="md:w-[65%]">
         <div class="bg-white rounded-lg p-4">
           <div class="text-xl font-semibold mb-2">Shipping Address</div>
           <div v-if="currentAddress && currentAddress.data">
-            <NuxtLink to="/address" class="flex items-center pb-2 text-blue-500 hover:text-red-400">
-              <Icon name="mdi:plus" size="18" class="mr-2" />
+            <NuxtLink
+              to="/address"
+              class="flex items-center pb-2 text-blue-500 hover:text-red-400"
+            >
+              <Icon
+                name="mdi:plus"
+                size="18"
+                class="mr-2"
+              />
               Update Address
             </NuxtLink>
             <div class="pt-2 border-t">
@@ -45,19 +55,36 @@
               </ul>
             </div>
           </div>
-          <NuxtLink v-else to="/address" class="flex items-center text-blue-500 hover:text-red-400">
-            <Icon name="mdi:plus" size="18" class="mr-2" />
+          <NuxtLink
+            v-else
+            to="/address"
+            class="flex items-center text-blue-500 hover:text-red-400"
+          >
+            <Icon
+              name="mdi:plus"
+              size="18"
+              class="mr-2"
+            />
             Add New Address
           </NuxtLink>
         </div>
-        <div id="Items" class="bg-white rounded-lg p-4 mt-4">
-          <div v-for="product in userStore.checkout" v-bind:key="product.id">
+        <div
+          id="Items"
+          class="bg-white rounded-lg p-4 mt-4"
+        >
+          <div
+            v-for="product in userStore.checkout"
+            v-bind:key="product.id"
+          >
             <CheckoutItem :product="product" />
           </div>
         </div>
       </div>
       <div class="md:w-[35%]">
-        <div id="PlaceOrder" class="bg-white rounded-lg p-4">
+        <div
+          id="PlaceOrder"
+          class="bg-white rounded-lg p-4"
+        >
           <div class="text-2xl font-extrabold mb-2">Summary</div>
           <div class="flex items-center justify-between my-4">
             <div class="">Total Shipping</div>
@@ -67,25 +94,37 @@
           <div class="flex items-center justify-between my-4">
             <div class="font-semibold">Total</div>
             <div class="text-2xl font-semibold">
-              $ <span class="font-extrabold">{{ total / 100 }}</span>
+              $
+              <span class="font-extrabold">{{ total / 100 }}</span>
             </div>
           </div>
           <form @submit.prevent="pay()">
-            <div class="border border-gray-500 p-2 rounded-sm" id="card-element" />
-            <p id="card-error" role="alert" class="text-red-700 text-center font-semibold" />
-            <button :disabled="isProcessing" type="submit"
+            <div
+              class="border border-gray-500 p-2 rounded-sm"
+              id="card-element"
+            />
+            <p
+              id="card-error"
+              role="alert"
+              class="text-red-700 text-center font-semibold"
+            />
+            <button
+              :disabled="isProcessing"
+              type="submit"
               class="mt-4 bg-gradient-to-r from-[#FE630C] to-[#FF3200] w-full text-white text-[21px] font-semibold p-1.5 rounded-full"
-              :class="isProcessing ? 'opacity-70' : 'opacity-100'">
-              <Icon v-if="isProcessing" name="eos-icons:loading" />
+              :class="isProcessing ? 'opacity-70' : 'opacity-100'"
+            >
+              <Icon
+                v-if="isProcessing"
+                name="eos-icons:loading"
+              />
               <div v-else>Place order</div>
             </button>
           </form>
         </div>
         <div class="bg-white rounded-lg p-4 mt-4">
           <div class="text-lg font-semibold mb-2 mt-2">AliExpress</div>
-          <p class="my-2">
-            AliExpress keeps your information and payment safe
-          </p>
+          <p class="my-2">AliExpress keeps your information and payment safe</p>
         </div>
       </div>
     </div>
@@ -93,144 +132,145 @@
 </template>
 
 <script setup>
-import { useUserStore } from '~/stores/user';
+  import { useUserStore } from '~/stores/user';
 
-definePageMeta({ middleware: 'auth' });
+  definePageMeta({ middleware: 'auth' });
 
-const userStore = useUserStore();
-const user = useSupabaseUser();
+  const userStore = useUserStore();
+  const user = useSupabaseUser();
 
-let stripe = null;
-let elements = null;
-let card = null;
-let total = ref(0);
-let clientSecret = null;
-const currentAddress = ref(null);
-const isProcessing = ref(false);
+  let stripe = null;
+  let elements = null;
+  let card = null;
+  let total = ref(0);
+  let clientSecret = null;
+  const currentAddress = ref(null);
+  const isProcessing = ref(false);
 
-onBeforeMount(async () => {
-  userStore.isLoading = true;
-  if (userStore.checkout.length < 1) {
-    return navigateTo('/shopping-cart');
-  }
-
-  total.value = 0.0;
-
-  if (!user.value) {
-    return navigateTo('/auth');
-  }
-
-  if (user.value) {
-    const address = await useFetch(`/api/get-address-by-user/${user.value.id}`);
-    console.log(address);
-    currentAddress.value = address;
-  }
-});
-
-onMounted(async () => {
-  isProcessing.value = true;
-
-  userStore.checkout.forEach((item) => {
-    total.value += item.price;
-  });
-});
-
-watch(
-  () => total.value,
-  () => {
-    if (total.value > 0) {
-      stripeInit();
+  onBeforeMount(async () => {
+    userStore.isLoading = true;
+    if (userStore.checkout.length < 1) {
+      return navigateTo('/shopping-cart');
     }
-  }
-);
 
-const stripeInit = async () => {
-  stripe = Stripe('pk_test_omlXjzDjv5JiiZriL2q7XT3d00PisYvFQu');
+    total.value = 0.0;
 
-  const res = await $fetch('/api/stripe/paymentintent', {
-    method: 'POST',
-    body: {
-      amount: total.value,
-    },
+    if (!user.value) {
+      return navigateTo('/auth');
+    }
+
+    if (user.value) {
+      const address = await useFetch(
+        `/api/get-address-by-user/${user.value.id}`
+      );
+      console.log(address);
+      currentAddress.value = address;
+    }
   });
-  clientSecret = res.client_secret;
 
-  elements = stripe.elements();
-  const style = {
-    base: {
-      fontSize: '18px',
-    },
-    invalid: {
-      fontFamily: 'Arial, sans-serif',
-      color: '#EE4B2B',
-      iconColor: '#EE4B2B',
-    },
+  onMounted(async () => {
+    isProcessing.value = true;
+
+    userStore.checkout.forEach((item) => {
+      total.value += item.price;
+    });
+  });
+
+  watch(
+    () => total.value,
+    () => {
+      if (total.value > 0) {
+        stripeInit();
+      }
+    }
+  );
+
+  const stripeInit = async () => {
+    stripe = Stripe('pk_test_omlXjzDjv5JiiZriL2q7XT3d00PisYvFQu');
+
+    const res = await $fetch('/api/stripe/paymentintent', {
+      method: 'POST',
+      body: {
+        amount: total.value,
+      },
+    });
+    clientSecret = res.client_secret;
+
+    elements = stripe.elements();
+    const style = {
+      base: {
+        fontSize: '18px',
+      },
+      invalid: {
+        fontFamily: 'Arial, sans-serif',
+        color: '#EE4B2B',
+        iconColor: '#EE4B2B',
+      },
+    };
+    card = elements.create('card', {
+      hidePostalCode: true,
+      style: style,
+    });
+
+    card.mount('#card-element');
+    card.on('change', function (event) {
+      document.querySelector('button').disabled = event.empty;
+      document.querySelector('#card-error').textContent = event.error
+        ? event.error.message
+        : '';
+    });
+
+    card.on('ready', () => {
+      isProcessing.value = false;
+      userStore.isLoading = false;
+    });
   };
-  card = elements.create('card', {
-    hidePostalCode: true,
-    style: style,
-  });
 
-  card.mount('#card-element');
-  card.on('change', function (event) {
-    document.querySelector('button').disabled = event.empty;
-    document.querySelector('#card-error').textContent = event.error
-      ? event.error.message
-      : '';
-  });
+  const pay = async () => {
+    if (!currentAddress.value?.data?.name) {
+      showError('Please add shipping address');
+      return;
+    }
+    isProcessing.value = true;
 
-  card.on('ready', () => {
-    isProcessing.value = false;
-    userStore.isLoading = false;
-  });
-};
+    const result = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: { card: card },
+    });
 
-const pay = async () => {
-  console.log(currentAddress.value);
-  if (!currentAddress.value?.data?.name) {
-    showError('Please add shipping address');
-    return;
-  }
-  isProcessing.value = true;
+    if (result.error) {
+      showError(result.error.message);
+      isProcessing.value = false;
+    } else {
+      await createOrder(result.paymentIntent.id);
+      userStore.cart = [];
+      userStore.checkout = [];
+      setTimeout(() => {
+        return navigateTo('/success');
+      }, 500);
+    }
+  };
 
-  const result = await stripe.confirmCardPayment(clientSecret, {
-    payment_method: { card: card },
-  });
+  const createOrder = async (stripeId) => {
+    await useFetch('/api/create-order', {
+      method: 'POST',
+      body: {
+        userId: user.value.id,
+        stripeId: stripeId,
+        name: currentAddress.value.data.name,
+        address: currentAddress.value.data.address,
+        zipcode: currentAddress.value.data.zipcode,
+        city: currentAddress.value.data.city,
+        country: currentAddress.value.data.country,
+        products: userStore.checkout,
+      },
+    });
+  };
 
-  if (result.error) {
-    showError(result.error.message);
-    isProcessing.value = false;
-  } else {
-    await createOrder(result.paymentIntent.id);
-    userStore.cart = [];
-    userStore.checkout = [];
+  const showError = (errorMsgText) => {
+    const errorMsg = document.querySelector('#card-error');
+    errorMsg.textContent = errorMsgText;
     setTimeout(() => {
-      return navigateTo('/success');
-    }, 500);
-  }
-};
-
-const createOrder = async (stripeId) => {
-  await useFetch('/api/create-order', {
-    method: 'POST',
-    body: {
-      userId: user.value.id,
-      stripeId: stripeId,
-      name: currentAddress.value.data.name,
-      address: currentAddress.value.data.address,
-      zipcode: currentAddress.value.data.zipcode,
-      city: currentAddress.value.data.city,
-      country: currentAddress.value.data.country,
-      products: userStore.checkout,
-    },
-  });
-};
-
-const showError = (errorMsgText) => {
-  const errorMsg = document.querySelector('#card-error');
-  errorMsg.textContent = errorMsgText;
-  setTimeout(() => {
-    errorMsg.textContent = '';
-  }, 4000);
-};
+      errorMsg.textContent = '';
+    }, 4000);
+  };
 </script>
